21b09d00cba2fa0afc8232aabeb38fb80fe472e6
Chris <zhris@zhr.is>
2024-11-17 11:43:48 -0600

    Initial Commit

---
+++ Feedback Assistant iOS/Feedback_Assistant_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Feedback_Assistant_iOSApp.swift
+//  Feedback Assistant iOS
+//
+//  Created by Chris on 11/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Feedback_Assistant_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
